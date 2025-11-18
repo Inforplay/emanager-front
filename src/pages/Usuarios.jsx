cdc51911804f803a642f9bff1769076e71e9fe07
netoresende <raimundogomes826@gmail.com>
2025-11-18 20:26:49 -0300

    ...

--- src/pages/Usuarios.jsx
+++ src/pages/Usuarios.jsx
@@ -1,12 +1,7 @@
 import { Button, Drawer, Form, Input, Popconfirm, Select, Table } from "antd";
-import {
-    useBuscarUsuario,
-    useCriarUsuario,
-    useDeletarUsuario,
-    useEditarUsuario,
-} from "../hooks/usuarioHooks";
+import { useBuscarUsuario, useCriarUsuario, useDeletarUsuario, useEditarUsuario } from "../hooks/usuarioHooks";
 import { BiPencil, BiTrash } from "react-icons/bi";
-import { useContext, useRef, useState } from "react";
+import { useContext, useState } from "react";
 import { AntContext } from "../contexts/AntProvider";
 import { useBuscarNivel } from "../hooks/nivelHooks";
 
@@ -21,25 +16,22 @@ const Usuarios = () => {
     const [verEditar, setVerEditar] = useState(false);
     const [formEditar] = Form.useForm();
     const [formCriar] = Form.useForm();
-    const formRef = useRef(null);
-    
-    
 
     function criar(dados) {
         criarUsuario(dados, {
             onSuccess: (response) => {
                 setVerCriar(false);
                 api[response.tipo]({
-                    description: response.mensagem,
-                });
+                    description: response.mensagem
+                })
                 formCriar.resetFields()
             },
             onError: (response) => {
                 api[response.tipo]({
-                    description: response.mensagem,
-                });
-            },
-        });
+                    description: response.mensagem
+                })
+            }
+        })
     }
 
     function editar(dados) {
@@ -47,58 +39,63 @@ const Usuarios = () => {
             onSuccess: (response) => {
                 setVerEditar(false);
                 api[response.tipo]({
-                    description: response.mensagem,
-                });
+                    description: response.mensagem
+                })
             },
             onError: (response) => {
                 api[response.tipo]({
-                    description: response.mensagem,
-                });
-            },
-        });
+                    description: response.mensagem
+                })
+            }
+        })
     }
 
     function deletar(id) {
         deletarUsuario(id, {
             onSuccess: (response) => {
                 api[response.tipo]({
-                    description: response.mensagem,
-                });
+                    description: response.mensagem
+                })
             },
             onError: (response) => {
                 api[response.tipo]({
-                    description: response.mensagem,
-                });
-            },
-        });
+                    description: response.mensagem
+                })
+            }
+        })
     }
 
     return (
         <div className="p-15">
             <div className="flex items-center justify-between mb-4">
-                <h1>Pagina Usuarios</h1>
-                <Button type="primary" onClick={() => setVerCriar(true)}>
-                    Novo usuario
-                </Button>
+                <h1>Pagina de Usuários</h1>
+                <Button type="primary" onClick={() => setVerCriar(true)}>Novo Usuário</Button>
             </div>
-            <Table dataSource={usuarios || []} rowKey={"id"}>
+            <Table
+                dataSource={usuarios || []}
+                rowKey={"id"}
+            >
                 <Table.Column
                     key={"id"}
                     dataIndex={"id"}
                     title={"ID"}
                     className="w-[50px]"
                 />
-                <Table.Column key={"nome"} dataIndex={"nome"} title={"Nome"} />
+                <Table.Column
+                    key={"nome"}
+                    dataIndex={"nome"}
+                    title={"Nome"}
+                />
                 <Table.Column
                     title={"Ações"}
                     className="w-[100px]"
-                    render={(_, usuario) => (
+                    render={(_, usuarios) => (
                         <div className="flex gap-3">
                             <BiPencil
                                 size={18}
                                 onClick={() => {
                                     formEditar.setFieldsValue({
-                                        ...usuario
+                                        ...usuarios
                                     });
                                     setVerEditar(true);
                                 }}
@@ -106,7 +103,7 @@ const Usuarios = () => {
                             <Popconfirm
                                 title="Aviso:"
                                 description="Deseja realmente apagar?"
-                                onConfirm={() => deletar(usuario.id)}
+                                onConfirm={() => deletar(usuarios.id)}
                                 okText="Sim"
                                 cancelText="Não"
                             >
@@ -122,7 +119,11 @@ const Usuarios = () => {
                 open={verCriar}
                 onClose={() => setVerCriar(false)}
             >
-                <Form layout="vertical" onFinish={criar} form = {formCriar}>
+                <Form
+                    layout="vertical"
+                    onFinish={criar}
+                    form={formCriar}
+                >
                     <Form.Item
                         label={"Nome"}
                         name={"nome"}
@@ -143,28 +144,25 @@ const Usuarios = () => {
                         name={"senha"}
                         rules={[{ required: true, message: "Campo obrigatório" }]}
                     >
-                        <Input.Password />
+                        <Input.Password/>
                     </Form.Item>
+
                     <Form.Item
                         label={"Nivel"}
                         name={"nivel_id"}
                         rules={[{ required: true, message: "Campo obrigatório" }]}
                     >
-                        <Select
-                            options={niveisCarregados? niveis.map(nivel =>{
-                                return {
-                                    value: nivel.id,
-                                    label: nivel.nome
-                                }
-                            }):[]
-                
+                       <Select
+                          options={niveisCarregados ? niveis.map(nivel => {
+                            return { 
+                                value : nivel.id, 
+                                label : nivel.nome
                             }
-                        />
+                          }) : []}  
+                       />
                     </Form.Item>
-
-                    <Button htmlType="submit" type="primary">
-                        Criar
-                    </Button>
+                    
+                    <Button htmlType="submit" type="primary">Criar</Button>
                 </Form>
             </Drawer>
 
@@ -173,10 +171,19 @@ const Usuarios = () => {
                 open={verEditar}
                 onClose={() => setVerEditar(false)}
             >
-                <Form layout="vertical" onFinish={editar} form={formEditar}>
-                    <Form.Item hidden name={"id"}>
+                <Form
+                    layout="vertical"
+                    onFinish={editar}
+                    form={formEditar}
+                   
+                >
+                    <Form.Item
+                        hidden
+                        name={"id"}
+                    >
                         <Input />
                     </Form.Item>
+
                     <Form.Item
                         label={"Nome"}
                         name={"nome"}
@@ -195,34 +202,31 @@ const Usuarios = () => {
                     <Form.Item
                         label={"Senha"}
                         name={"senha"}
-                        
+                        rules={[{ required: true, message: "Campo obrigatório" }]}
                     >
-                        <Input.Password />
+                        <Input.Password/>
                     </Form.Item>
+
                     <Form.Item
                         label={"Nivel"}
                         name={"nivel_id"}
                         rules={[{ required: true, message: "Campo obrigatório" }]}
                     >
-                        <Select
-                            options={niveisCarregados? niveis.map(nivel =>{
-                                return {
-                                    value: nivel.id,
-                                    label: nivel.nome
-                                }
-                            }):[]
-                
+                       <Select
+                          options={niveisCarregados ? niveis.map(nivel => {
+                            return { 
+                                value : nivel.id, 
+                                label : nivel.nome
                             }
-                        />
+                          }) : []}  
+                       />
                     </Form.Item>
 
-                    <Button htmlType="submit" type="primary">
-                        Editar
-                    </Button>
+                    <Button htmlType="submit" type="primary">Editar</Button>
                 </Form>
             </Drawer>
         </div>
     );
-};
+}
 
 export default Usuarios;
